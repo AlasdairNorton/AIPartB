104f2bb032a7947e923b10f58787d4aff2248996
AlasdairNorton <AlasdairNorton@users.noreply.github.com>
2014-05-29 06:41:47 +0100

    Final Update

--- src/aiproj/ourCode/Board.java
+++ src/aiproj/ourCode/Board.java
@@ -1,9 +1,12 @@
-package aiproj.ourCode;
+package aiproj.fencemaster.ajnorton;
 
 /* Alasdair Norton (ajnorton)
  * Mostafa Rizk (mrizk) */
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+import aiproj.fencemaster.Move;
 import aiproj.fencemaster.Piece;
 
 public class Board implements Piece{
@@ -15,84 +18,25 @@ public class Board implements Piece{
 	 * computation
 	 */
 	private int arraySize;
-	
 	/* Array containing all the positions on the board
 	 * First dimension is y (rows), second is x (position in row) */
 	private Position[][] nodes;
-	
 	/* Arraylist containing all clusters (continuous groups of same-coloured pieces) */
 	private ArrayList<Cluster> clusters;
-	
-	/* Creates a new board defined by the standard input 
-	 * Not used in Part B, not guaranteed to be compatible with rest
-	 * of code */
-	public Board (){
-		/* Initialise instance variables */
-		clusters = new ArrayList<Cluster>();		
-		Scanner sc = new Scanner(System.in);
-		int i, j;
-		
-		/* Get size from input */
-		if(sc.hasNextInt()){
-			this.size = sc.nextInt();
-		} else {
-			System.out.println("Error");
-			System.exit(1);
-		}	
-		
-		/* Set arraysize, initialise array */
-		this.arraySize = size+1;
-		nodes = new Position[2*arraySize-1][2*arraySize-1];
-		
-		/* For each cell in the array, initialise to INVALID piece */
-		for(i=0;i<2*arraySize-1;i++){
-			for(j=0;j<2*arraySize-1;j++){
-				nodes[i][j] = new Position(i,j,INVALID);
-			}
-		}
+	private ArrayList<Cluster> clustRemoveList;
 
-		/* For each cell in the board */
-		for(i=0;i<2*arraySize-1;i++){
-			for(j=Math.max(0, i-arraySize+1); j< Math.min(arraySize+i, 2*arraySize-1) ;j++){
-				if(i==0 || i== 2*arraySize-2
-						|| j==Math.max(0, i-arraySize+1)
-						|| j==Math.min(arraySize+i, 2*arraySize-1)-1){
-					/* Skip (Used to initialise invalid, but doing that for all non-board cells now)
-					 * Should update loop to ignore these cells at some point */
-				}else{
-					if(sc.hasNext()){
-						/* Otherwise add a new position with colour given by the input */
-						int color=0;
-						if(sc.next().charAt(0) == '-'){
-							color = EMPTY;
-						} else if(sc.next().charAt(0) == 'B'){
-							color = BLACK;
-						} else if(sc.next().charAt(0) == 'W'){
-							color = WHITE;
-						} else {
-							System.out.println("Syntax Error");
-							System.exit(1);
-						}
-						nodes[i][j] = new Position(i,j,color);
-					} else {
-						System.out.println("Error");
-						System.exit(1);
-					}
-				}
-			}
-		}
-		sc.close();
-	}
-	
+
+
+
 	/* Creates a new empty board, with sidelength size */
 	public Board (int size){
 		/* Initialise instance variables */
 		clusters = new ArrayList<Cluster>();	
+		clustRemoveList = new ArrayList<Cluster>(0);
 		int i, j;
 		
 		/* Get size from input */
 		this.size = size;
-
 		
 		/* Set arraysize, initialise array */
 		this.arraySize = size+1;
@@ -151,7 +95,6 @@ public class Board implements Piece{
 		Cluster newCluster = new Cluster(node.getColour());
 		ArrayList<Position> toAdd = new ArrayList<Position>(0);
 		Position tempNode;
-		
 		newCluster.addNode(node);
 		node.setParentCluster(newCluster);
 		toAdd.addAll(node.getAdjacents(this));
@@ -166,6 +109,37 @@ public class Board implements Piece{
 			}
 		}
 		clusters.add(newCluster);
+	}
+	
+
+	public void mergeClusters(Cluster c1, Cluster c2){
+		c1.setDidChange(true);
+		c1.getNodes().addAll(c2.getNodes());
+		for(Position p: c2.getNodes()){
+			p.setParentCluster(c1);
+		}
+		clustRemoveList.add(c2);
+	}
+	
+	public void updateNearMove(Move m){
+		Position p = nodes[m.Row+1][m.Col+1];
+		Cluster currCluster = new Cluster(p.getColour());
+		currCluster.addNode(p);
+		p.setParentCluster(currCluster);
+		clusters.add(currCluster);
+		for(Position adj: p.getAdjacents(this)){
+			if(adj.getColour()==p.getColour()){
+				mergeClusters(adj.getParentCluster(), currCluster);
+				currCluster = adj.getParentCluster();
+			}
+		}
+		
+		for(Cluster c: clustRemoveList){
+			clusters.remove(c);
+		}
+		clustRemoveList.clear();
+		
+		p.setParentCluster(currCluster);
 	}
 	
 	public void clearClusters(){
@@ -194,34 +168,40 @@ public class Board implements Piece{
 	
 	public int testWin(){
 		// Taken from Controller.java, from Part A
-		Boolean[] win = {false, false, false, false};
-		this.clearClusters();
-		this.makeClusters();
+		Boolean[] win = {false, false, false, false};	
 		for(Cluster clust: this.getClusters()){
-		/* For each cluster, test win conditions */
-			if(clust.getColour() == BLACK){
-				if(clust.testTripod(this)){
-					win[0]=true;
+			/* If the cluster has updated since the last check for win.
+			 * didChange is always true for all clusters in the players'
+			 * board representation, so every cluster is checked there
+			 * but in the minimax search, only clusters immediately affected
+			 * by a change are searched.
+			 */
+			if(clust.getDidChange()){
+				/* For each cluster, test win conditions */
+				if(clust.getColour() == BLACK){
+					if(clust.testTripod(this)){
+						win[0]=true;
+					}
+					
+					if(clust.testLoop(this)){
+						win[1]=true;
+					}
 				}
-				
-				if(clust.testLoop(this)){
-					win[1]=true;
-				}
-			}
-			if(clust.getColour() == WHITE){
-				if(clust.testTripod(this)){
-					win[2]=true;
-				}
-				
-				if(clust.testLoop(this)){
-					win[3]=true;
+				if(clust.getColour() == WHITE){
+					if(clust.testTripod(this)){
+						win[2]=true;
+					}
+					
+					if(clust.testLoop(this)){
+						win[3]=true;
+					}
 				}
 			}
 		}
 		if((win[0] || win[1]) && !win[2] && !win[3]){
 			// Black Wins
 			return BLACK;
-		}
+		}	
 		if(!win[0] && !win[1] && (win[2] || win[3])){
 			// White wins
 			return WHITE;
@@ -235,7 +215,8 @@ public class Board implements Piece{
 			return INVALID;
 		}
 		return INVALID;
-	}
+	}	
+	
 	
 	public int getSize() {
 		return size;
@@ -267,4 +248,5 @@ public class Board implements Piece{
 	public void setArraySize(int arraySize) {
 		this.arraySize = arraySize;
 	}
+	
 }
